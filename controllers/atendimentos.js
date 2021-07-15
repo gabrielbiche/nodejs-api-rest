42c6f893a217fe870cfc38d3b0284aa7777f2bfc
Gabriel <gabrielsbiche@gmail.com>
2021-07-15 16:50:46 -0300

    conectado servidor ao BD

--- controllers/atendimentos.js
+++ controllers/atendimentos.js
@@ -1,10 +1,11 @@
+const Atendimento = require('../models/atendimentos')
+
 module.exports = app => {
     app.get("/atendimentos", (req, res) => res.send('Você esta na rota de atendimentos, method get'))
 
     app.post("/atendimentos", (req, res) => {
-        console.log(req.body);
+        const atendimento = req.body
+        Atendimento.adiciona(atendimento)
         res.send('Voce esta na rota de atendimento, method post')
-    });
-
-
+    })
 }
